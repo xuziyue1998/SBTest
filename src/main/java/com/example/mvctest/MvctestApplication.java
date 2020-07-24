9b15a3ba1efb455808e09fc0f77496999ef5655d
xuziyue1998 <xuziyue@icloud.com>
2020-07-24 20:30:12 +0800

    Initial commit

---
+++ src/main/java/com/example/mvctest/MvctestApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvctest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvctestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvctestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mvctest/MvctestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvctest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvctestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
